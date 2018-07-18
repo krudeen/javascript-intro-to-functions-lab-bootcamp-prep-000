4cfdff4ce81951a1dfcad08a454484e65a4efb14
Kyle Rudeen <krudeen@gmail.com>
2018-07-18 20:06:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function shout(string) {
   return string.toUpperCase()
 }
@@ -11,10 +12,26 @@ function logShout(string) {
 }
 
 function logWhisper(string) {
+=======
+function shout(string)  {
+  return string.toUpperCase()
+}
+
+function whisper(string)  {
+  return string.toLowerCase()
+}
+
+function logShout(string)  {
+  console.log('HELLO')
+}
+
+function logWhisper(string)  {
+>>>>>>> 8ea2300e65ba22ca1121d68e08193f12a1ca76e4
   console.log('hello')
 }
 
 function sayHiToGrandma(string) {
+<<<<<<< HEAD
   if (string === string.toLowerCase()) 
     return "I can\'t hear you!"
   if (string === string.toUpperCase())
@@ -22,3 +39,7 @@ function sayHiToGrandma(string) {
   if ("I Love you, Grandma.")
     return "I love you, too."
   }
+=======
+  return string.toLowerCase
+}
+>>>>>>> 8ea2300e65ba22ca1121d68e08193f12a1ca76e4
